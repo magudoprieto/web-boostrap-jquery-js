410103488ef9b48d8768a9da15a0a4cb14e2544d
origin <magudoprieto@gmail.com>
2018-12-26 19:11:20 +0100

    perfeccionando desarrollo.html

--- js/desarrollo.js
+++ js/desarrollo.js
@@ -17,20 +17,3 @@ function cierreMenu(e){
 	$('.fondodos').fadeOut(1000);
 }
 
-//evento abrir menu desarrollo
-
-var btnapp = document.querySelector('.app');
-btnapp.addEventListener('click',abrirDesarrollo);
-
-function abrirDesarrollo(){
-    $('.negro').fadeIn(1000).css('display','block');
-}
-
-//evento cerrar menu desarrollo
-
-var btnCerrarApp = document.querySelector('.cerrarapp');
-btnCerrarApp.addEventListener('click', cierreApp);
-
-function cierreApp(e){
-	$('.negro').fadeOut(1000);
-}
