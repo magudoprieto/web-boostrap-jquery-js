9862b7a1b3949cb0f57786b966aa22c51bb022a9
origin <magudoprieto@gmail.com>
2018-12-28 19:52:59 +0100

    creacion de diseño y unificacion de js

--- js/menu.js
+++ js/menu.js
@@ -4,7 +4,7 @@ var btnMenu = document.querySelector('#selector');
 btnMenu.addEventListener('click', cambiarMenu);
 
 function cambiarMenu(e){
-	$('.menu').fadeIn(1000).css('display','block');
+	$('.fondodos').fadeIn(1000).css('display','block');
 }
 
 
@@ -14,6 +14,6 @@ var btnClose = document.querySelector('#close');
 btnClose.addEventListener('click', cierreMenu);
 
 function cierreMenu(e){
-	$('.menu').fadeOut(1000);
+	$('.fondodos').fadeOut(1000);
 }
 
